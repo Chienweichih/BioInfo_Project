2b9744d5484ff1819114d8f2146b805db5276aa4
Chienweichih <chienweichih@gmail.com>
2015-11-18 13:05:04 +0800

    search protien

--- excuse/views.py
+++ excuse/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from models import Excuse
 
+import urllib2
+import json
+
+REST_URL = "http://data.bioontology.org"
+API_KEY = "a4a36c45-883f-432e-9dec-5db68f11f767"
+
+def get_json(url):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('Authorization', 'apikey token=' + API_KEY)]
+    return json.loads(opener.open(url).read())
+
 def home(request):
-    excuse = Excuse.objects.all().order_by('?')[0]
-    return render(request, "index.html", {'excuse': excuse})
+    term = "protein"
+    search_result = get_json(REST_URL + "/search?q=" + term)["collection"]
+
+    return render(request, "index.html", {'excuse': search_result})
